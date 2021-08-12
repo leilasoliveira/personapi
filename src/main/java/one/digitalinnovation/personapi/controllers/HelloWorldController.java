533438ea2d5d1d87050f91e712fad291506d083b
Leila Farias <leila.s.oliveira@hotmail.com>
2021-08-12 18:17:27 -0300

    Hello World endpoint

---
+++ src/main/java/one/digitalinnovation/personapi/controllers/HelloWorldController.java
@@ -0,0 +1,16 @@
+package one.digitalinnovation.personapi.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class HelloWorldController {
+
+    @GetMapping
+    private String helloWorld() {
+        return "Olá Heroku! Sou a aplicação Olho no Preço!";
+    }
+
+}
